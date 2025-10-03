ff3e77fac48603c6403e579f69ee2359d37ed37c
Silenthack3r <108166146+Silenthack3r@users.noreply.github.com>
2025-10-03 15:36:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -406,4 +406,4 @@ def report():
 #To run say false if you want
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(host="0.0.0.0", port=port)
